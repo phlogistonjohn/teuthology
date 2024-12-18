314fde0d69caae2c87b1545acb0fe6237e44dc02
John Mulligan <jmulligan@redhat.com>
2024-12-18 10:57:07 -0500

    xxx: choose different pool based on config
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- teuthology/machines/pool.py
+++ teuthology/machines/pool.py
@@ -1,16 +1,24 @@
 
 import logging
 
+from teuthology.config import config
 from teuthology.machines.base import MachinePool
 from teuthology.machines.lock_server_pool import LockServerMachinePool
+from teuthology.machines.sqlite_pool import SqliteMachinePool
 
 
 log = logging.getLogger(__name__)
 
 
 def from_config() -> MachinePool:
-    log.info("Using lock server machine pool")
-    return LockServerMachinePool()
+    if not config.machine_pool or config.machine_pool.startswith(
+        'lock_server'
+    ):
+        mpool = LockServerMachinePool()
+    elif config.machine_pool.startswith('sqlite:'):
+        mpool = SqliteMachinePool()
+    log.info("Using machine pool: %s", mpool.description())
+    return mpool
 
 
 def auto_pool(ctx=None, pool: MachinePool | None = None) -> MachinePool:
